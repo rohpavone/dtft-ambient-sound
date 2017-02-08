8553f378561b396d59756a83b41780f9e7f88c3f
Rohan Pavone <rohan.pavone@gmail.com>
2017-02-08 01:06:46 -0500

    Reorganize methods

--- test.c
+++ test.c
@@ -2,7 +2,8 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <complex.h>
-double dtft(double *, int, float, double);
+double complex dtft(double *, int, float, double);
+double complex * produce_dtft(double * points, int num_points, double sample_period, float min_freq, float max_freq, int num_frequencies);
 
 int main(void)
 {
@@ -14,53 +15,45 @@ int main(void)
 	// variable declaration
 	double * points;
 	double * sums;
-	int num_points = 10; // only 128 points for now
-	float min = 1/(2*M_PI), max = 1/(2*M_PI);
-	
-	int freq = 20; // 20 frequency columns
-
-	float freqinc = (max - min)/freq;
-
-	points = (double *) malloc(sizeof(double) * num_points);
-	sums = (double *) malloc(sizeof(double) * freq);
-	
+	int num_points = 1024; // only 128 points for now
+	points = (double *) malloc(sizeof(double) * num_points*2);
 	// point collection
 
 	fprintf(stdout, "Collecting points\n");
 	unsigned int counter = 0;
-	for(double i = -M_PI; i < M_PI; i += M_PI/num_points)
+        double max_val = 0;
+        double temp;
+	for(double i = -1; i < 1; i += (double)1/num_points)
 	{
-		points[counter] = sin(2*M_PI*i);
-		fprintf(fpoints, "%f,", points[counter]);
+		points[counter] = sin(4*M_PI*i) + 4*cos(8*M_PI*i);
+                temp = sqrt(pow(points[counter], 2));
+                if(temp > max_val) max_val = temp;
+                fprintf(stdout, "%d: %f\n", counter,points[counter]);
 		counter++;
 	}
-/*
-	counter = 0;
-	fprintf(stdout, "Fourier transform\n");
-	for(int i = 0; i < freq; i ++)
-	{
-		sums[i] = 0;
-		for(float j = min + freqinc * i; j < min + freqinc * (i + 1); j += freqinc/10)
-		{
-			sums[i] += dtft(points, num_points, j, M_PI/num_points);
-		}
-	}
-	fprintf(stdout, "RESULTS:\n");
-	for(int i = 0; i < freq; i++)
-	{
-	
-		fprintf(fout, "%f Hz: %f\n", min + freqinc*i, sums[i]);
-	}*/
+        double complex * results = produce_dtft(points, counter, (double)1/num_points, -10, 10, 4096);
 }
 
-double dtft(double * function, int length, float freq, double increase)
+double complex * produce_dtft(double * points, int num_points, double sample_period, float min_freq, float max_freq, int num_frequencies) {
+        double complex * sums = (double complex *) malloc(sizeof(double complex) * num_frequencies);
+
+	float freqinc = (max_freq - min_freq)/num_frequencies;
+
+	for(int i = 0; i < num_frequencies; i ++)
+	{
+		sums[i] = dtft(points, num_points*2, min_freq+freqinc *i, (double)1/num_points);
+	}
+        return sums;
+}
+
+double complex dtft(double * function, int length, float freq, double sample_period)
 {
-	fprintf(stdout, "DTFT\n");
 	// real value only
 	double complex sum = 0;
-	for(unsigned int i = 0; i < length; i++)
+	for(int i = -length/2; i < length/2; i++)
 	{
-		sum += function[i] * cexp(-2 * M_PI * (i * increase) * freq * I) * increase;
+                // multiply to normalize
+		sum += function[i+length/2] * cexp(-2 * M_PI * (i) * freq * sample_period * I) *sample_period;
 	}
-	return creal(sum);
+	return sum;
 }
